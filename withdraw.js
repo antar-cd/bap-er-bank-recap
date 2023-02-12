505f156083d6ef54b25da4666c8cd779cf76c5d7
antar-cd <sagar.chandradas1994@gmail.com>
2023-02-13 02:24:43 +0600

    add balance element

--- withdraw.js
+++ withdraw.js
@@ -9,11 +9,20 @@ document.getElementById('btn-withdraw').addEventListener('click',function(){
      const previousWithdrawString= withDrawTotalElement.innerText;
      const previousWithdraw = parseFloat(previousWithdrawString);
      //console.log(previousWithdraw);
+    //step-5: 
 
      const withdrawTotal= previousWithdraw + withDrawAmount;
      withDrawTotalElement.innerText= withdrawTotal;
+    //step-6:
 
+    const balanceElement= document.getElementById('balance-total');
+    const previousBalanceTotalString= balanceElement.innerText;
+    const prevDepositeTotal =parseFloat(previousBalanceTotalString);
 
+        //step-7:
+
+    const newBalanceTotal= prevDepositeTotal - withDrawAmount;
+    balanceElement.innerText= newBalanceTotal;
      withdrawField.value ='';
      //const withDrawTotalElement=document.getElementById('withdraw-total');
      
